525247245e11b6685b7178bfb4c61b8d0103a608
jakkie <jakubkielbasiewicz@gmail.com>
2025-07-07 15:26:19 +0200

    storage logic

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod s3;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use s3::{upload_to_s3};
